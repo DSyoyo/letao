bfbf6942c3dd17831e9abfbd59dd0a8e7f248a76
DSyoyo <851237336@qq.com>
2018-06-18 12:38:44 +0800

    完成了主页面的一些公用功能

--- public/admin/js/common.js
+++ public/admin/js/common.js
@@ -12,3 +12,43 @@ $(document).ajaxStop(function () {
   // 结束进度条
   NProgress.done();
 });
+
+
+
+
+// 主页公共类
+// 侧边栏的显示隐藏
+$(".icon-list").on("click",function(){
+  $(".lt-aside").toggleClass("now");
+  $(".lt-info").toggleClass("now");
+})
+
+// 一级，二级菜单的显示与隐藏
+$(".list").prev().on("click",function(){
+  $(".list").slideToggle();
+})
+
+// 退出登录
+$(".icon-logout").on("click",function(){
+  // alert("heh")
+  // 模态框显示
+  $("#logoutModal").modal("show");
+
+  $(".btn-logout").off().on("click",function(){
+    // 发送ajax请求
+    $.ajax({
+      type:"get",
+      url: "/employee/employeeLogout",
+      success: function(info){
+        console.log(info);
+        // 若退出成功，跳转登录页面
+        if (info.success) {
+          location.href = "login.html";
+        }
+      }
+    })
+  })
+
+
+ 
+})
